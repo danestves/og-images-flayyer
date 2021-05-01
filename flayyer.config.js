1df4b4cb8bee970ce2fd7dcc036dffc6127ae92f
Daniel Esteves <daniel@seeed.us>
2021-04-30 21:32:47 -0400

    feat(config): adding more fields for flayyer config

--- flayyer.config.js
+++ flayyer.config.js
@@ -4,11 +4,15 @@ const { config } = require("@flayyer/flayyer-types");
 require("dotenv").config();
 
 module.exports = config({
+  deck: "danestves",
   engine: "react-typescript",
   key: process.env.FLAYYER_KEY,
-  deck: "danestves",
-
-  // Optionals
-  name: "danestves og-images service",
   description: "Created with create-flayyer-app",
+  homepage:
+    "https://danestves.com/en/blog/how-to-create-awesome-ogimages-using-react-components-ckmsgs7cwqj0o0c817wvtpojn",
+  license: "MIT",
+  marketplace: true,
+  name: "danestves og-images service",
+  private: false,
+  repository: "git@github.com:danestves/og-images-flayyer.git",
 });
